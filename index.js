cebd6337a2d4a56e62d118c6825f18eb5da95dd3
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2025-04-14 23:56:47 -0400

    feat: improved styling and themes

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
-function update_copyright() {
-    let cr = document.getElementById("copyright");
-    let year = new Date().getFullYear();
-    cr.innerHTML = "Copyright © " + year + " Rei Meguro";
+function updateCopyright() {
+  // Set the copyright to the current year
+  let cr = document.getElementById("copyright");
+  let year = new Date().getFullYear();
+  cr.innerHTML = "©" + year + " Rei Meguro";
 }
 
+function initializeThemeToggle() {
+  // Set the initial theme based on the prefered color scheme meta
+  const modeIcon = document.getElementById("mode-toggle");
 
+  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    document.body.setAttribute("data-theme", "light");
+    modeIcon.firstChild.classList.add("fa-sun");
+  } else {
+    document.body.setAttribute("data-theme", "dark");
+    modeIcon.firstChild.classList.add("fa-moon");
+  }
 
+  // Set handler for theme toggle
+  modeIcon.addEventListener("click", function () {
+    const body = document.body;
 
-function mobile_scroll() {
-    let mobile_cards = document.querySelectorAll("section.projects .cards .card");
-    if (
-        !window.matchMedia("(max-aspect-ratio: 5/4)").matches ||
-        !window.matchMedia("(hover: none)").matches
-    ) return
-
-    var FADE_START = 0.3;
-    var FADE_TILL = 0.6;
-    var KEEP_TILL = 0.8;
-
-    var MIN_IMG_OPACITY = 0.2;
-
-    // mobile with no hover
-    for (let i=0; i<mobile_cards.length; i++) {
-        let card = mobile_cards[i];
-        let viewdist = -(card.getBoundingClientRect().top - window.innerHeight + 20);
-        let viewratio = viewdist/window.innerHeight;
-
-        if (viewratio < 0 || viewratio > 1)
-            continue;
-
-        let cardimg = card.children[0];
-        let cardtext = card.children[1];
-
-        if (viewratio < FADE_START){    // no text
-            cardimg.style.opacity = 1;
-            cardtext.style.opacity = 0;
-
-        } else if (viewratio < FADE_TILL) { // fade into text
-            cardimg.style.opacity = 1 - (1 - MIN_IMG_OPACITY)*(viewratio - FADE_START)/FADE_START;
-            cardtext.style.opacity = (viewratio - FADE_START)/FADE_START;
-
-        } else if (viewratio < KEEP_TILL) { // keep
-            cardimg.style.opacity = MIN_IMG_OPACITY;
-            cardtext.style.opacity = 1;
-
-        } else {    // fade back to no text
-            cardimg.style.opacity = MIN_IMG_OPACITY + (1 - MIN_IMG_OPACITY)*(viewratio - KEEP_TILL)/(1 - KEEP_TILL);
-            cardtext.style.opacity = 1 - (viewratio - KEEP_TILL)/(1 - KEEP_TILL);
-        };
+    if (body.getAttribute("data-theme") === "dark") {
+      body.setAttribute("data-theme", "light");
+      modeIcon.firstChild.classList.replace("fa-moon", "fa-sun");
+    } else {
+      body.setAttribute("data-theme", "dark");
+      modeIcon.firstChild.classList.replace("fa-sun", "fa-moon");
     }
+  });
 }
 
-window.addEventListener("scroll", mobile_scroll);
+function onLoad() {
+  // Entry function when page loads
+  updateCopyright();
+  initializeThemeToggle();
+}
